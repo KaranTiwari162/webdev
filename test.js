5895416a121bac1eec061a6a97a49ab61f81207a
webkaran18 <webdevkaran18@gmail.com>
2026-05-17 11:44:44 +0530

    first commit

---
+++ test.js
@@ -0,0 +1,16 @@
+alert("hello world");
+
+console.log("Your Code is Cute...")
+
+var a = prompt("Enter your number")
+var istrue = confirm("r u sure u want to go back")
+
+if (istrue){ 
+console.log("u r computer is blasting")
+}
+
+else{
+   console.log ("u r computer is not blasting")
+}
+
+console.log("your number is "  +  a)
