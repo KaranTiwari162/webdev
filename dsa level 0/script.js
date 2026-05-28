ec724f3d44f92865d8a244933280617a0648ddfa
webkaran18 <webdevkaran18@gmail.com>
2026-05-28 21:51:46 +0530

    dsa practice

---
+++ dsa level 0/script.js
@@ -0,0 +1,55 @@
+// let n =10;
+// for (let i = 0; i < n; i++) {
+//     console.log(i);
+    
+// }
+
+// let n =10;
+// for (let i = 0; i < n; i++) {
+//     if (i%2 ==0) {
+//         console.log(i);
+//     }
+// }
+//  let n = 5;
+// for (
+//     let i = 1; i <= 10; i++) {
+//     let multiply = n * i
+//     console.log(multiply);
+// }
+
+// let fact = 1;
+
+// for (let i = 1; i <= 5; i++) {
+//     fact = fact * i;
+// }
+
+// console.log("Factorial of 5 is:", fact);
+
+
+// let C = 50
+// let F = 9 / 5*(C) + 32
+// console.log(F)
+
+// let n = 11
+// if(n%2==0){
+//     console.log("Number is even " )
+// }
+// else {
+//     console.log("Number is odd ")
+// }
+
+// let r = 5;
+// let area = 3.14 * r * r;
+// console.log(area)
+
+// let a = 2;
+// let b = 1;
+// let c = 0;
+
+// c = a;
+// a = b;
+// b = c;
+// console.log(a);
+// console.log(b);
+
+
---
+++ video 74/script.js
@@ -0,0 +1,5 @@
+let button = document.getElementById("btn")
+button.addEventListener("mouseenter",()=>{
+    // alert("this is clicked")
+    document.querySelector(".box").innerHTML="gooooooooooo"
+})
