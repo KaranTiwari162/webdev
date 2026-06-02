f3a8e52e72b79814724d7dc7662f42b9b4f14f54
webkaran18 <webdevkaran18@gmail.com>
2026-06-02 18:25:27 +0530

    catch erroorrrr

---
+++ video 79/script.js
@@ -0,0 +1,21 @@
+let a = Number(prompt("enter a number"))
+let b = Number(prompt("enter a number"))
+if (isNaN(a)|| isNaN(b)) {
+    throw SyntaxError("r u ullu ")
+}
+let sum = (a+b) 
+function main(){
+try {
+    console.log(sum*x)
+    return true
+} catch (error) {
+   console.log("eerrorrrr aaagyaaa laadleeey x to chli gyi") 
+   return false
+}
+// finally used aata hai catch and error k return krne k baad bhi chlta hai
+finally{
+    console.log("filese are being closed")
+}
+}
+
+let c= main()
