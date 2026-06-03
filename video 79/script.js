65a857c1a221087a49db3e3020e884a850303d27
webkaran18 <webdevkaran18@gmail.com>
2026-06-03 22:13:14 +0530

    changes

--- video 79/script.js
+++ video 79/script.js
@@ -12,7 +12,7 @@ try {
    console.log("eerrorrrr aaagyaaa laadleeey x to chli gyi") 
    return false
 }
-// finally used aata hai catch and error k return krne k baad bhi chlta hai
+// finally used aata hai catch and error k0 return krne k baad bhi chlta hai
 finally{
     console.log("filese are being closed")
 }
