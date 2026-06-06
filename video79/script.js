2b64585ab1c32baa98432ed37339ebba2d92d760
Karan Tiwari <webdevkaran18@gmail.com>
2026-06-06 20:27:49 +0530

    Add files via upload

---
+++ video79/script.js
@@ -0,0 +1,21 @@
+let a = Number(prompt("enter a number"))
+let b = Number(prompt("enter a number"))
+if (isNaN(a)|| isNaN(b)) {
+    throw SyntaxError("r u ullu ")
+}
+let sum = (a+b) 
+function main(){
+try {
+    console.log(sum*x)
+    return true
+} catch (error) {
+   console.log("eerrorrrrrrr aaagyaaa laadleeey x to chli gyi") 
+   return false
+}
+// finally used aata hai catch and error ko return krne k baad bhi chlta hai
+finally{
+    console.log("files are being closed")
+}
+}
+
+let c= main()
